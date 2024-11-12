9b7fabdeef8df6ba6ee3b80705097815ed1ff316
karan-raval <karanraval424@gmail.com>
2024-11-12 14:34:46 +0530

    My Portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/karan-raval.github.io/', // base path of the repository
+  base: '/',
   // other configurations
 });
